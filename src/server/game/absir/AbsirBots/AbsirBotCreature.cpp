48565fdde3d8a4ec096444b9fdef123b52da76de
absir@qq.com <absir@qq.com>
2015-07-03 17:56:52 +0800

    优化数据结构

---
+++ src/server/game/absir/AbsirBots/AbsirBotCreature.cpp
@@ -0,0 +1,136 @@
+#include "AbsirBotCreature.h"
+#include "WorldSocket.h"
+#include "GroupMgr.h"
+
+static AbsirBotAI *createBotAI(AbsirBotCreature *botCreature) {
+	// 
+	uint8 cls = botCreature->getClass();
+	AbsirBotAI *botAI = new AbsirBotAI();
+
+	botAI->initCreature(botCreature);
+	return botAI;
+};
+
+/*
+class AB_BotSocket : public WorldSocket {
+AB_BotSocket() : WorldSocket::WorldSocket(NULL) {
+};
+void Start() override {};
+void SendPacket(WorldPacket const& packet) {};
+};
+
+static WorldSocket *BOT_SOCKET = new AB_BotSocket();
+static std::shared_ptr<WorldSocket> BOT_SOCKET_PTR(BOT_SOCKET);
+*/
+
+class AB_BotSession : public WorldSession {
+
+public:
+	AB_BotSession() : WorldSession(0, NULL, SEC_PLAYER, 0, 0, LOCALE_enUS, 0, true) {
+	}
+
+	AbsirBotCreature *botCreature;
+
+private:
+};
+
+static CharacterCreateInfo *BOT_CHARACTER = new CharacterCreateInfo();
+
+AbsirBotCreature::AbsirBotCreature() : Unit(false), Creature(), Player(new AB_BotSession()) {
+	AB_BotSession *botSession = (AB_BotSession*)GetSession();
+	botSession->SetPlayer(this);
+	botSession->botCreature = this;
+}
+
+AbsirBotCreature::~AbsirBotCreature() {
+	if (m_botAi) {
+		delete m_botAi;
+	}
+
+	WorldSession *session = GetSession();
+	if (session) {
+		delete session;
+	}
+}
+
+AbsirBotCreature *AbsirBotCreature::createBotData(Player *player, Map* map, uint32 phaseMask, uint32 entry, float x, float y, float z, float ang, CreatureData const* data, uint32 vehId)
+{
+	AbsirBotCreature *botCreature = new AbsirBotCreature();
+	Creature *creature = botCreature;
+	if (creature->Create(sObjectMgr->GenerateLowGuid(HIGHGUID_UNIT), map, phaseMask, entry, x, y, z, ang, data, vehId)) {
+		Group *group = player->GetGroup();
+		if (group == NULL) {
+			group = new Group();
+			if (group->Create(player)) {
+				sGroupMgr->AddGroup(group);
+			}
+			else {
+				delete group;
+				group = NULL;
+			}
+		}
+
+		if (group != NULL) {
+			if (!group->isRaidGroup() && group->GetMembersCount() >= MAXGROUPSIZE) {
+				group->ConvertToRaid();
+			}
+
+			botCreature->m_owerPlayer = player;
+			if (group->AddMember(botCreature->getBotPlayer())) {
+				botCreature->followOwnerStats();
+				creature->AddToWorld();
+				return botCreature;
+			}
+		}
+	}
+
+	creature->RemoveFromWorld();
+	delete creature;
+	return NULL;
+}
+
+Player *AbsirBotCreature::getBotPlayer()
+{
+	if (m_botPlayer == NULL) {
+		m_botPlayer = this;
+		m_botPlayer->Create(m_owerPlayer->GetGUID(), BOT_CHARACTER);
+		m_botPlayer->AddToWorld();
+		SetOwnerGUID(m_owerPlayer->GetGUID());
+		m_owerPlayer->absirGameFlag |= AB_FLAG_HAS_BOT;
+		m_botAi = createBotAI(this);
+	}
+
+	return m_botPlayer;
+}
+
+void AbsirBotCreature::followOwnerStats()
+{
+	SetLevel(m_owerPlayer->getLevel());
+}
+
+void AbsirBotCreature::AddToWorld()
+{
+	bool inWold = IsInWorld();
+	if (!inWold) {
+		sObjectAccessor->AddObject((Player *)this);
+	}
+
+	Creature::AddToWorld();
+	Player::AddToWorld();
+}
+
+void AbsirBotCreature::RemoveFromWorld()
+{
+	bool inWold = IsInWorld();
+	if (inWold) {
+		Group *group = GetGroup();
+		if (group) {
+			group->RemoveMember(GetGUID(), GROUP_REMOVEMETHOD_LEAVE);
+		}
+
+		sObjectAccessor->RemoveObject((Player *)this);
+	}
+
+	Creature::RemoveFromWorld();
+	Player::RemoveFromWorld();
+}
---
+++ src/server/game/absir/AbsirBots/AbsirBotCreature.h
@@ -0,0 +1,113 @@
+#ifndef AB_BOT_CREATURE
+#define AB_BOT_CREATURE
+
+#include "AbsirGame.h"
+
+class AbsirBotCreature;
+
+class AbsirBotAI  {
+public:
+	void initCreature(AbsirBotCreature *botCreature) {
+		m_botCreature = botCreature;
+	};
+	
+private:
+	AbsirBotCreature *m_botCreature;
+};
+
+/*
+Creature.h
+class Creature : public virtual Unit, public virtual GridObject<Creature>, public MapObject
+Player.h
+class Player : public virtual Unit, public virtual GridObject<Player>
+*/
+class AbsirBotCreature : public virtual Creature, public virtual Player
+{
+public:
+	static AbsirBotCreature *createBot(Player *player, Unit *unit) {
+		return createBotData(player, unit->GetMap(), PHASEMASK_NORMAL, unit->GetEntry(), unit->GetPositionX(), unit->GetPositionY(), unit->GetPositionZ(), unit->GetOrientation());
+	};
+	static AbsirBotCreature *createBotData(Player *player, Map* map, uint32 phaseMask, uint32 entry, float x, float y, float z, float ang, CreatureData const* data = nullptr, uint32 vehId = 0);
+	
+	AbsirBotCreature();
+	~AbsirBotCreature();
+	Player *getOwnerPlayer() { return m_owerPlayer; };
+	AbsirBotAI *getBotAI() { return m_botAi; };
+	Player *getBotPlayer();
+
+	void followOwnerStats();
+
+	void AddToWorld() override;
+	void RemoveFromWorld() override;
+	
+	void SetObjectScale(float scale) {
+		Creature::SetObjectScale(scale);
+	};
+	void Update(uint32 time) {
+		Creature::Update(time);
+	};
+	bool CanAlwaysSee(WorldObject const* obj) const {
+		return Creature::CanAlwaysSee(obj);
+	};
+	uint32 GetShieldBlockValue() const {
+		return Creature::GetShieldBlockValue();
+	};
+	bool HasSpell(uint32 spellID) const {
+		return Creature::HasSpell(spellID);
+	};
+	bool SetDisableGravity(bool disable, bool packetOnly = false) {
+		return Creature::SetDisableGravity(disable, packetOnly);
+	};
+	bool SetCanFly(bool enable, bool packetOnly = false) {
+		return Creature::SetCanFly(enable, packetOnly);
+	};
+	bool SetWaterWalking(bool enable, bool packetOnly = false) {
+		return Creature::SetWaterWalking(enable, packetOnly);
+	};
+	bool SetFeatherFall(bool enable, bool packetOnly = false) {
+		return Creature::SetFeatherFall(enable, packetOnly);
+	};
+	bool SetHover(bool enable, bool packetOnly = false) {
+		return Creature::SetHover(enable, packetOnly);
+	};
+	void setDeathState(DeathState s) {
+		return Player::setDeathState(s);
+	};
+	bool UpdateStats(Stats stat) {
+		return Creature::UpdateStats(stat);
+	};
+	bool UpdateAllStats() {
+		return Creature::UpdateAllStats();
+	};
+	void UpdateResistances(uint32 school) {
+		return Creature::UpdateResistances(school);
+	};
+	void UpdateArmor() {
+		return Creature::UpdateArmor();
+	};
+	void UpdateMaxHealth() {
+		return Creature::UpdateMaxHealth();
+	};
+	void UpdateMaxPower(Powers power) {
+		return Creature::UpdateMaxPower(power);
+	};
+	void UpdateAttackPowerAndDamage(bool ranged = false) {
+		Creature::UpdateAttackPowerAndDamage(ranged);
+	};
+	void CalculateMinMaxDamage(WeaponAttackType attType, bool normalized, bool addTotalPct, float& minDamage, float& maxDamage) {
+		Creature::CalculateMinMaxDamage(attType, normalized, addTotalPct, minDamage, maxDamage);
+	};
+	bool CanFly() const {
+		return Creature::CanFly();
+	};
+	void SetTarget(ObjectGuid guid) {
+		Creature::SetTarget(guid);
+	};
+
+private:
+	Player *m_owerPlayer = NULL;
+	AbsirBotAI *m_botAi = NULL;
+	Player *m_botPlayer = NULL;
+};
+
+#endif
